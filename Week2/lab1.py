57d48e67ae4f462cc41642b433b16ccfb7ec7481
beloroista <belorovista@gmail.com>
2015-09-17 19:16:39 -0400

    9/17

--- Week2/lab1.py
+++ Week2/lab1.py
@@ -5,28 +5,72 @@ PopulationAndDensity = "http://boxnumbertwo.com/PittsburghData/Population_and_De
 
 fhand = urllib.urlopen(PopulationAndDensity)
 
-pop1940 = dict()
+pop1940=dict()
+pop1950=dict()
+pop1960=dict()
+pop1970=dict()
+pop1980=dict()
+pop1990=dict()
+pop2000=dict()
+pop2010=dict()
+
+
 try:
 	reader = csv.reader(fhand)
 	next(reader, None)  # skip the headers
 	for row in reader:
-		print "add here"
+		pop1940[row[0]]=row[2].replace(',', '')
+		pop1950[row[0]]=row[3].replace(',', '')
+		pop1960[row[0]]=row[4].replace(',', '')
+		pop1970[row[0]]=row[5].replace(',', '')
+		pop1980[row[0]]=row[6].replace(',', '')
+		pop1990[row[0]]=row[7].replace(',', '')
+		pop2000[row[0]]=row[8].replace(',', '')
+		pop2010[row[0]]=row[9].replace(',', '')
 finally:
 	fhand.close()
-	
+
+
+poptotal = dict()
+poptotal[1940]=0
+poptotal[1950]=0
+poptotal[1960]=0
+poptotal[1970]=0
+poptotal[1980]=0
+poptotal[1990]=0
+poptotal[2000]=0
+poptotal[2010]=0	
 popDifference4050 = dict()	
 
 for key in pop1940:
-	print "add here"
+	poptotal[1940]=poptotal[1940]+int(pop1940[key])	
+for key in pop1950:
+	poptotal[1950]=poptotal[1950]+int(pop1950[key])
+for key in pop1960:
+	poptotal[1960]=poptotal[1960]+int(pop1960[key])
+for key in pop1970:
+	poptotal[1970]=poptotal[1970]+int(pop1970[key])
+for key in pop1980:
+	poptotal[1980]=poptotal[1980]+int(pop1980[key])
+for key in pop1990:
+	poptotal[1990]=poptotal[1990]+int(pop1990[key])
+for key in pop2000:
+	poptotal[2000]=poptotal[2000]+int(pop2000[key])
+for key in pop2010:
+	poptotal[2010]=poptotal[2010]+int(pop2010[key])
+
+
 	
 
-print "Difference between 1940 - 1950: "
-print "Difference between 1950 - 1960: "
-print "Difference between 1960 - 1970: "
-print "Difference between 1970 - 1980: "
-print "Difference between 1980 - 1990: "
-print "Difference between 1990 - 2000: "
-print "Difference between 2000 - 2010: "
+	
+
+print "Difference between 1940 - 1950: " + str(poptotal[1950]-poptotal[1940])
+print "Difference between 1950 - 1960: " + str(poptotal[1960]-poptotal[1950])
+print "Difference between 1960 - 1970: " + str(poptotal[1970]-poptotal[1960])
+print "Difference between 1970 - 1980: " + str(poptotal[1980]-poptotal[1970])
+print "Difference between 1980 - 1990: " + str(poptotal[1990]-poptotal[1980])
+print "Difference between 1990 - 2000: " + str(poptotal[2000]-poptotal[1990])
+print "Difference between 2000 - 2010: " + str(poptotal[2010]-poptotal[2000])
 
 #Difference between 1940 - 1950:  5148
 #Difference between 1950 - 1960:  -61563
@@ -36,13 +80,13 @@ print "Difference between 2000 - 2010: "
 #Difference between 1990 - 2000:  -36352
 #Difference between 2000 - 2010:  -27823
 
-print "#Difference between 1940 - 1950 for Mount Washington: "
-print "#Difference between 1950 - 1960 for Mount Washington: "
-print "#Difference between 1960 - 1970 for Mount Washington: "
-print "#Difference between 1970 - 1980 for Mount Washington: "
-print "#Difference between 1980 - 1990 for Mount Washington: "
-print "#Difference between 1990 - 2000 for Mount Washington: "
-print "#Difference between 2000 - 2010 for Mount Washington: "
+print "#Difference between 1940 - 1950 for Mount Washington: " + str(int(pop1950["Mount Washington"])-int(pop1940["Mount Washington"]))
+print "#Difference between 1950 - 1960 for Mount Washington: " + str(int(pop1960["Mount Washington"])-int(pop1950["Mount Washington"]))
+print "#Difference between 1960 - 1970 for Mount Washington: " + str(int(pop1970["Mount Washington"])-int(pop1960["Mount Washington"]))
+print "#Difference between 1970 - 1980 for Mount Washington: " + str(int(pop1980["Mount Washington"])-int(pop1970["Mount Washington"]))
+print "#Difference between 1980 - 1990 for Mount Washington: " + str(int(pop1990["Mount Washington"])-int(pop1980["Mount Washington"]))
+print "#Difference between 1990 - 2000 for Mount Washington: " + str(int(pop2000["Mount Washington"])-int(pop1990["Mount Washington"]))
+print "#Difference between 2000 - 2010 for Mount Washington: " + str(int(pop2010["Mount Washington"])-int(pop2000["Mount Washington"]))
 #Difference between 1940 - 1950 for Mount Washington:  -953
 #Difference between 1950 - 1960 for Mount Washington:  -1645
 #Difference between 1960 - 1970 for Mount Washington:  -2628
@@ -51,13 +95,13 @@ print "#Difference between 2000 - 2010 for Mount Washington: "
 #Difference between 1990 - 2000 for Mount Washington:  -822
 #Difference between 2000 - 2010 for Mount Washington:  -1079
 
-print "#Difference between 1940 - 1950 for North Oakland: "
-print "#Difference between 1950 - 1960 for North Oakland: "
-print "#Difference between 1960 - 1970 for North Oakland: "
-print "#Difference between 1970 - 1980 for North Oakland: "
-print "#Difference between 1980 - 1990 for North Oakland: "
-print "#Difference between 1990 - 2000 for North Oakland: "
-print "#Difference between 2000 - 2010 for North Oakland: "
+print "#Difference between 1940 - 1950 for  and bottom: " + str(int(pop1950["North Oakland"])-int(pop1940["North Oakland"]))
+print "#Difference between 1950 - 1960 for North Oakland: " + str(int(pop1960["North Oakland"])-int(pop1950["North Oakland"]))
+print "#Difference between 1960 - 1970 for North Oakland: " + str(int(pop1970["North Oakland"])-int(pop1960["North Oakland"]))
+print "#Difference between 1970 - 1980 for North Oakland: " + str(int(pop1980["North Oakland"])-int(pop1970["North Oakland"]))
+print "#Difference between 1980 - 1990 for North Oakland: " + str(int(pop1990["North Oakland"])-int(pop1980["North Oakland"]))
+print "#Difference between 1990 - 2000 for North Oakland: " + str(int(pop2000["North Oakland"])-int(pop1990["North Oakland"]))
+print "#Difference between 2000 - 2010 for North Oakland: " + str(int(pop2010["North Oakland"])-int(pop2000["North Oakland"]))
 #Difference between 1940 - 1950 for North Oakland:  1936
 #Difference between 1950 - 1960 for North Oakland:  -610
 #Difference between 1960 - 1970 for North Oakland:  1213
@@ -67,13 +111,13 @@ print "#Difference between 2000 - 2010 for North Oakland: "
 #Difference between 2000 - 2010 for North Oakland:  694
 
 	
-print "#Difference between 1940 - 1950 for Shadyside: "
-print "#Difference between 1950 - 1960 for Shadyside: "
-print "#Difference between 1960 - 1970 for Shadyside: "
-print "#Difference between 1970 - 1980 for Shadyside: "
-print "#Difference between 1980 - 1990 for Shadyside: "
-print "#Difference between 1990 - 2000 for Shadyside: "
-print "#Difference between 2000 - 2010 for Shadyside: "
+print "#Difference between 1940 - 1950 for Shadyside: " + str(int(pop1950["Shadyside"])-int(pop1940["Shadyside"]))
+print "#Difference between 1950 - 1960 for Shadyside: " + str(int(pop1960["Shadyside"])-int(pop1950["Shadyside"]))
+print "#Difference between 1960 - 1970 for Shadyside: " + str(int(pop1970["Shadyside"])-int(pop1960["Shadyside"]))
+print "#Difference between 1970 - 1980 for Shadyside: " + str(int(pop1980["Shadyside"])-int(pop1970["Shadyside"]))
+print "#Difference between 1980 - 1990 for Shadyside: " + str(int(pop1990["Shadyside"])-int(pop1980["Shadyside"]))
+print "#Difference between 1990 - 2000 for Shadyside: " + str(int(pop2000["Shadyside"])-int(pop1990["Shadyside"]))
+print "#Difference between 2000 - 2010 for Shadyside: " + str(int(pop2010["Shadyside"])-int(pop2000["Shadyside"]))
 #Difference between 1940 - 1950 for Shadyside:  1599
 #Difference between 1950 - 1960 for Shadyside:  -1102
 #Difference between 1960 - 1970 for Shadyside:  -2329
@@ -84,9 +128,19 @@ print "#Difference between 2000 - 2010 for Shadyside: "
 
 totalDiff20101940 = dict()  
 ### USE THIS TO SORT, Returns a list (array) ###
-totalDiff20101940 = sorted(totalDiff20101940.items(), key=lambda x:x[1])
+for key in pop2010:
+	totalDiff20101940[key]=int(pop2010[key])-int(pop1940[key])	
+	
 print "Top 10 - Worst"
+totalDiff20101940 = sorted(totalDiff20101940.items(), key=lambda x:x[1] )
+for i in range(10):
+	print totalDiff20101940[i]
+	
 print "Top 10 - Best"	
+
+for i in range(len(totalDiff20101940)-10,len(totalDiff20101940)):
+	print totalDiff20101940[i]
+
 
 	
 #Top 10 - Worst
